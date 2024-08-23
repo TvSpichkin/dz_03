f9a194d4aeaddeb895bf16cb6fb5146793499e94
TvSpichkin <ericlen@mail.ru>
2024-08-23 23:29:02 +0300

    Фикс

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import {postsRout} from "./features/posts";
 import {testRout} from "./features/testing";
 
 export const app = express(); // Определение экспресс приложения
-app.use(express.json()) // Cоздание свойств-объектов тела и вопросов во всех запросах
+app.use(express.json()); // Cоздание свойств-объектов тела и вопросов во всех запросах
 
 
 app.get("/", (запр: Request, отв: Response) => {
@@ -13,6 +13,6 @@ app.get("/", (запр: Request, отв: Response) => {
     отв.send("Servak rabotaet");
 }); // Проверка успешного запуска сервера
 
-app.use(SET.PATH.BLOGS, blogsRout) // Подключение маршрутизатора сетевых журналов
-app.use(SET.PATH.POSTS, postsRout) // Подключение маршрутизатора записей
-app.use(SET.PATH.TESTING, testRout) // Подключение маршрутизатора тестирования
+app.use(SET.PATH.BLOGS, blogsRout); // Подключение маршрутизатора сетевых журналов
+app.use(SET.PATH.POSTS, postsRout); // Подключение маршрутизатора записей
+app.use(SET.PATH.TESTING, testRout); // Подключение маршрутизатора тестирования
